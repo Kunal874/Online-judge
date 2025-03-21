af7fc3e74b2d9e8f81df0557aa0f08510412a7b9
Kunal874 <144765495+Kunal874@users.noreply.github.com>
2025-03-21 12:42:43 +0530

    Add files via upload

---
+++ client/src/Components/Subcard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL} from "./Url.js"
+
+export default function Subcard({id,name,code,verdict,lang,time}){
+    const Nav = useNavigate();
+    const handleClick = async ()=>{
+        const desc = await axios.post(BACKEND_URL+"getstatement",{id})
+        const description=desc.data;
+        sessionStorage.setItem('fromsub',true);
+        Nav(`/problem/${id}`,{state: {name,description,code,lang}})
+    }
+
+    const map = new Map();
+    map.set('cpp','C++'); map.set('py','Python 3'); map.set('c','C');
+    const Lang=map.get(lang)
+    return(
+    <div className="subcard" onClick={handleClick}>
+        <div className="sub-element">{time}</div>
+        <div className="sub-element"> {name} </div>
+        <div className="sub-element"> {Lang} </div>
+        <div className="sub-element"> {verdict} </div>
+    </div>
+    )
+}
